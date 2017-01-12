5c7e46cc60bbf7b5fc12d270da67488c9da2ae32
Michał Kurkowski <michalkurkowski93@gmail.com>
2017-01-12 23:20:48 +0100

    poprawka crawlera

--- lib/utils/crawler_pyszne_torun.rb
+++ lib/utils/crawler_pyszne_torun.rb
@@ -10,7 +10,8 @@ restaurants_hash = {}
   website_html.css('div.yd-jig-service-dl').each do |restaurant|
     address_suffix = restaurant.css('li.yd-jig-service-dl-info-name').css('a').attr('href').to_s[1..-1] #adres www.restauracji na pyszne.pl
     unless restaurants_hash.keys.include? address_suffix
-      current_restaurant_address = 'https://pyszne.pl/' << address_suffix << '/informacje#!'
+      current_restaurant_address_general = 'https://pyszne.pl/' << address_suffix
+      current_restaurant_address = current_restaurant_address_general  + '/informacje#!'
       current_restaurant_html = Nokogiri::HTML open current_restaurant_address
       div_with_detailed_info = current_restaurant_html.css('div.yd-jig-serviceinfo-details.yd-grid')
       detailed_address = current_restaurant_html.css('dd.adr')
@@ -20,7 +21,48 @@ restaurants_hash = {}
       delivery_price = delivery_node.to_s.scan(/(?:Koszt dostawy:.*?)(\d+,\d+)/).flatten.first.gsub(',', '.')
       minimal_delivery_price = delivery_node.to_s.scan(/(?:Dowóz:.*?)(\d+,\d+)/).flatten.first.gsub(',', '.')
       free_delivery = delivery_node.to_s.scan(/(?:Dostawa za darmo.*)(\d+,\d+)/).try(:flatten).try(:first).try(:gsub, ',', '.')
+
+      current_restaurant_general_html =  Nokogiri::HTML open current_restaurant_address_general
+      meals_arr = []
+
+      meals_ugly_text = current_restaurant_general_html
+        .css('#yd-application-data')
+        .to_s
+        .scan(/Yd\.predefined\[\"meals\"\](.*?)Yd\.predefined/m)
+        .flatten
+        .first
+        .gsub(';', '')
+        .gsub('=', '')
+
+      parsed_meals = JSON.parse(meals_ugly_text)
+      # puts parsed_meals.awesome_inspect plain: true
+
+      meals = []
+      parsed_meals.try(:each) do |category|
+        _meals = category['meals']
+        # puts _meals
+        _meals.try(:each) do |_meal|
+          meal = {
+            name: _meal['name'],
+            description: _meal['description'].gsub('\n', ' ')
+          }
+          sizes = []
+          _meal['sizes'].try(:each) do |_size|
+            sizes.push({
+              name: _size['name'],
+              cost: _size['cost']
+            })
+          end
+          meal[:sizes] = sizes
+          meals.push meal
+        end
+      end
+
+      # puts meals.awesome_inspect plain: true
+
+
       current_hash = restaurants_hash[address_suffix] = {
+        meals: meals,
         name: restaurant.css('li.yd-jig-service-dl-info-name').css('a').text.strip,  #nazwa restauracji
         picture: restaurant.css('div.yd-jig-service-dl-logo').css('img').attr('src').to_s[2..-1].strip,  #obrazek
         meal_types: restaurant.css('li.yd-jig-service-dl-info-food').text,  # rodzaje serwowanych potraw
