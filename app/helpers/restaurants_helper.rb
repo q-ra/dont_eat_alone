7ac840219340463ca052d2693ed731beb98f27cf
Michał Kurkowski <michalkurkowski93@gmail.com>
2017-01-13 22:39:29 +0100

    w trakcie prac nad widokiem restauracji

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,5 +1,14 @@
 module RestaurantsHelper
-  def resized_pyszne_image restaurant
-    'http://' << restaurant.picture_address.gsub('155-100', '310-200')
+  def pyszne_link link
+    'http://pyszne.pl/' + link
+  end
+
+  def todays_opening_closing restaurant
+    days = %w(mo tu we th fr sa su)
+    day_number = Time.now.wday.to_i
+    opening_closing = restaurant.opening_closing
+    opening = opening_closing[days[day_number] + '_opening']
+    closing = opening_closing[days[day_number] + '_closing']
+    opening << ' - ' << closing
   end
 end
